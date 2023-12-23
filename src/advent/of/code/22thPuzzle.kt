714d7ebb44e4cda5373d1b859632ca75685b3230
Pacso <palko9711@gmail.com>
2023-12-23 11:02:45 +0100

    add 22th solution

--- src/advent/of/code/22thPuzzle.kt
+++ src/advent/of/code/22thPuzzle.kt
@@ -1,6 +1,7 @@
 package advent.of.code
 
 import utils.Util
+import utils.println
 import java.util.*
 
 object `22thPuzzle` {
@@ -14,8 +15,7 @@ object `22thPuzzle` {
     private var resultSecond = 0L;
 
 
-//    private var xMap: MutableList<MutableList<String>> = arrayListOf();
-//    private var yMap: MutableList<MutableList<String>> = arrayListOf();
+    private var cbSet: MutableMap<String, MutableList<String>> = mutableMapOf();
 
     fun solve() {
         val startingTime = Date();
@@ -36,147 +36,136 @@ object `22thPuzzle` {
 
     fun first() {
 
-        val bricks: MutableList<Brick> = arrayListOf();
+        var bricks: MutableList<Brick> = arrayListOf();
+        var lineNumber = 0L;
         input.forEach { line ->
-            bricks.add(parseLine(line));
+            bricks.add(parseLine(line, lineNumber));
+            lineNumber++
         }
 
-        val xMap: MutableList<MutableList<String>> =
-            initMap(getBiggestXCoordinate(bricks), getBiggestZCoordinate(bricks))
-        val yMap: MutableList<MutableList<String>> =
-            initMap(getBiggestYCoordinate(bricks), getBiggestZCoordinate(bricks));
+        bricks = fall(bricks);
 
-        fillMap(xMap, bricks, "x");
-        fillMap(yMap, bricks, "y");
-
-        tiltMapXSouth(xMap, bricks)
-
-        Util.saveMapToFile(xMap, "xmap")
+        result = countDisintegratableBricks(bricks)
     }
 
     fun second() {
 
+
+        var bricks: MutableList<Brick> = arrayListOf();
+        var lineNumber = 0L;
+        input.forEach { line ->
+            bricks.add(parseLine(line, lineNumber));
+            lineNumber++
+        }
+
+        bricks = fall(bricks);
+
+        resultSecond = countSecond(bricks)
     }
 
-    private fun tiltMapXSouth(
-        mapX: MutableList<MutableList<String>>,
-        bricks: MutableList<Brick>
-    ): MutableList<MutableList<String>> {
+    private fun countSecond(bricks: MutableList<Brick>): Long {
+        val overlaps: MutableList<MutableList<Brick>> = arrayListOf();
+        bricks.forEach { brick ->
+            overlaps.add(bricks.minus(brick).filter { sb -> isOverlap(sb, brick) }.toMutableList())
+        }
 
+        val chainReactionBricks: MutableList<Brick> = arrayListOf()
 
-        for (i in mapX.size - 1 downTo 0) {
-            val chars = mapX[i].toSet().filter { c -> !c.equals(".") && !c.equals("-") };
-            chars.forEach { char ->
-                val charIndexesStart = bricks[char.toInt()].start.x
-                val charIndexesEnd = bricks[char.toInt()].end.x
-
-                var canMove = true;
-                var maxMove = mapX.size;
-                for (j in charIndexesStart..charIndexesEnd) {
-                    var k = 1;
-
-                    while ((i + k) < mapX.size && canMove) {
-                        if (mapX[i + k - 1][j.toInt()] == char && mapX[i + k][j.toInt()] == ".") {
-                            k++;
-                        } else {
-                            canMove = false;
-                        }
-                    }
-                    if (k < maxMove) {
-                        maxMove = k;
-                    }
-                }
-                for (j in charIndexesStart..charIndexesEnd) {
-                    if (mapX[i + maxMove - 1][j.toInt()] == char && mapX[i + maxMove][j.toInt()] == ".") {
-                        mapX[i + maxMove - 1][j.toInt()] = "."
-                        mapX[i + maxMove][j.toInt()] = char
-                    }
-                }
+        val brickIds = bricks.map { b -> b.id }.toMutableList();
+        overlaps.forEach{ o ->
+            if (o.size == 1) {
+                brickIds.removeIf { o[0].id == it }
             }
         }
 
-        return mapX;
+        chainReactionBricks.addAll(bricks.filter { b -> !brickIds.contains(b.id) }.toMutableList())
+
+        chainReactionBricks.forEach{cb ->
+            cbSet.set(cb.id, arrayListOf());
+            simulateFallForPt2(bricks.filter { b -> b.id != cb.id }.toMutableList(), cb.id)
+        }
+
+        return cbSet.map { cb -> cb.value.size }.sum().toLong();
     }
 
-    private fun fillMap(map: MutableList<MutableList<String>>, bricks: MutableList<Brick>, xy: String = "x") {
-        var brickId = 0L;
-        bricks.forEach { b ->
-            for (i in b.start.z..b.end.z) {
-                var start = 0L;
-                var end = 0L;
-                if (xy == "x") {
-                    start = b.start.x;
-                    end = b.end.x;
+    private fun simulateFallForPt2(bricks: MutableList<Brick>, cbId: String): MutableList<Brick> {
+        val sortedBricks = bricks.map { Brick(it.id, Coordinate3D(it.start.x,it.start.y,it.start.z,), Coordinate3D(it.end.x,it.end.y,it.end.z,)) }.toMutableList();
+        sortedBricks.forEach { brick ->
+            var done = false;
+            while (!done) {
+                if ((brick.start.z - 1 > 0) && sortedBricks.minus(brick).all { sb -> !isOverlap(sb, brick) }) {
+                    brick.start.z -= 1;
+                    brick.end.z -= 1;
+                    if (!cbSet[cbId]!!.contains(brick.id)) {
+                        cbSet[cbId]!!.add(brick.id);
+                    }
                 } else {
-                    start = b.start.y;
-                    end = b.end.y;
-                }
-                while (start <= end) {
-                    map[map.size - 1 - i.toInt()][start.toInt()] = "" + brickId;
-                    start++;
+                    done = true;
                 }
             }
-            brickId++;
+        }
+        return sortedBricks.toMutableList();
+    }
+
+    private fun fall(bricks: MutableList<Brick>): MutableList<Brick> {
+        val sortedBricks = bricks.sortedBy { b -> b.start.z }
+        sortedBricks.forEach { brick ->
+            var done = false;
+            while (!done) {
+                if ((brick.start.z - 1 > 0) && sortedBricks.minus(brick).all { sb -> !isOverlap(sb, brick) }) {
+                    brick.start.z -= 1;
+                    brick.end.z -= 1;
+                } else {
+                    done = true;
+                }
+            }
+        }
+        return sortedBricks.toMutableList();
+    }
+
+    private fun countDisintegratableBricks(bricks: MutableList<Brick>): Long {
+        val overlaps: MutableList<MutableList<Brick>> = arrayListOf();
+        bricks.forEach { brick ->
+            overlaps.add(bricks.minus(brick).filter { sb -> isOverlap(sb, brick) }.toMutableList())
         }
 
-        map[map.size-1] = map[map.size-1].map { "-" }.toMutableList()
+        var brickIds = bricks.map { b -> b.id }.toMutableList();
+        overlaps.forEach{ overlaps ->
+            if (overlaps.size == 1) {
+                brickIds.removeIf { overlaps[0].id == it }
+            }
+        }
+        return brickIds.size.toLong();
+    }
+
+    private fun isOverlap(brick1: Brick, brick2: Brick): Boolean {
+        return ((isXOverlap(brick1, brick2)
+                && isYOverlap(brick1, brick2)))
+                && (brick1.start.z <= (brick2.end.z - 1) && (brick1.end.z) >= brick2.start.z - 1);
+    }
+
+    private fun isXOverlap(brick1: Brick, brick2: Brick): Boolean {
+        return brick1.start.x <= brick2.end.x && brick1.end.x >= brick2.start.x
+    }
+
+    private fun isYOverlap(brick1: Brick, brick2: Brick): Boolean {
+        return brick1.start.y <= brick2.end.y && brick1.end.y >= brick2.start.y
     }
 
 
-    private fun parseLine(line: String): Brick {
+    private fun parseLine(line: String, lineNumber: Long): Brick {
         val coords = line.split("~");
         val startCoords = coords[0].split(",");
         val endCoords = coords[1].split(",");
         return Brick(
+            lineNumber.toString(),
             Coordinate3D(startCoords[0].toLong(), startCoords[1].toLong(), startCoords[2].toLong()),
             Coordinate3D(endCoords[0].toLong(), endCoords[1].toLong(), endCoords[2].toLong())
         )
     }
 
-    private fun getBiggestXCoordinate(bricks: MutableList<Brick>): Long {
-        var max = 0L;
-        bricks.forEach { b ->
-            if (b.end.x > max) {
-                max = b.end.x;
-            }
-        }
-        return max;
-    }
-
-    private fun getBiggestYCoordinate(bricks: MutableList<Brick>): Long {
-        var max = 0L;
-        bricks.forEach { b ->
-            if (b.end.y > max) {
-                max = b.end.y;
-            }
-        }
-        return max;
-    }
-
-    private fun getBiggestZCoordinate(bricks: MutableList<Brick>): Long {
-        var max = 0L;
-        bricks.forEach { b ->
-            if (b.end.z > max) {
-                max = b.end.z;
-            }
-        }
-        return max;
-    }
-
-    fun initMap(xy: Long, z: Long): MutableList<MutableList<String>> {
-        val map: MutableList<MutableList<String>> = arrayListOf();
-        for (i in 0..z) {
-            map.add(arrayListOf());
-            for (j in 0..xy) {
-                map[i.toInt()].add(".")
-            }
-        }
-        return map;
-    }
-
-
     data class Coordinate3D(var x: Long, var y: Long, var z: Long);
 
-    data class Brick(var start: Coordinate3D, var end: Coordinate3D);
+    data class Brick(var id: String, var start: Coordinate3D, var end: Coordinate3D);
 
 }
