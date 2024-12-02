32edc6a6f5dfe702d363e098243be60ee1a97160
Pacso <palko9711@gmail.com>
2024-12-02 07:00:09 +0100

    Add 2024 / 2 result

--- src/advent/of/code/main.kt
+++ src/advent/of/code/main.kt
@@ -2,8 +2,9 @@ package advent.of.code
 
 import advent.of.code.y2024.*
 
-const val DAY = "1";
+const val DAY = "2";
 
 fun main() {
     if (p1.DAY == DAY) p1.solve();
+    if (p2.DAY == DAY) p2.solve();
 }
---
+++ src/advent/of/code/y2024/p2.kt
@@ -0,0 +1,120 @@
+package advent.of.code.y2024
+
+import advent.of.code.Reader
+import java.util.*
+import kotlin.math.abs
+
+object p2 {
+    const val YEAR = "2024";
+    const val DAY = "2";
+
+    private var input: MutableList<String> = arrayListOf();
+
+    private var firstResult = 0;
+
+    private var secondResult = 0L;
+
+    fun solve() {
+        val startingTime = Date();
+        input.addAll(Reader.readInput(YEAR, DAY));
+
+        first();
+
+        second();
+
+        println("first: $firstResult");
+        println("second: $secondResult");
+
+
+        println(startingTime);
+        println(Date());
+
+    }
+
+    private fun first() {
+        var unsafe = 0;
+
+        input.forEach { line ->
+
+
+            var li = line.split(" ");
+
+            for (i in 0..<li.size) {
+
+                if (li[0].toInt() < li[1].toInt()) {
+                    if (i < li.size - 1) {
+                        if (li[i].toInt() == (li[i + 1].toInt() - 1) || li[i].toInt() == (li[i + 1].toInt() - 2) || li[i].toInt() == (li[i + 1].toInt() - 3)) {
+
+                        } else {
+                            unsafe++;
+                            break;
+                        }
+                    }
+
+                } else {
+                    if (i < li.size - 1) {
+                        if (li[i].toInt() == (li[i + 1].toInt() + 1) || li[i].toInt() == (li[i + 1].toInt() + 2) || li[i].toInt() == (li[i + 1].toInt() + 3)) {
+
+                        } else {
+                            unsafe++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        firstResult = input.size - unsafe;
+    }
+
+
+    private fun second() {
+
+        var safe = 0;
+
+        input.forEach { line ->
+
+
+            for (i in 0..<line.split(" ").size) {
+                var li = line.split(" ").filterIndexed { index, s -> index != i }
+                var strli = li.toString();
+                var usfl: MutableList<String> = arrayListOf();
+
+
+                for (i in 0..<li.size) {
+
+                    if (li[0].toInt() < li[1].toInt()) {
+                        if (i < li.size - 1) {
+                            if (li[i].toInt() == (li[i + 1].toInt() - 1) || li[i].toInt() == (li[i + 1].toInt() - 2) || li[i].toInt() == (li[i + 1].toInt() - 3)) {
+
+                            } else {
+                                usfl.add(strli)
+                                break;
+                            }
+                        }
+
+                    } else {
+                        if (i < li.size - 1) {
+                            if (li[i].toInt() == (li[i + 1].toInt() + 1) || li[i].toInt() == (li[i + 1].toInt() + 2) || li[i].toInt() == (li[i + 1].toInt() + 3)) {
+
+                            } else {
+                                usfl.add(strli)
+                                break;
+                            }
+                        }
+
+                    }
+
+                }
+                if (!usfl.contains(strli)) {
+                    safe++
+                    break;
+                }
+
+            }
+        }
+
+        secondResult = safe.toLong();
+    }
+}
