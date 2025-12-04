cd4a65cab704c6cbd5bbb6585e56fed576537bf3
Palkovics Gábor <gabor.palkovics@kh.hu>
2025-12-04 06:29:20 +0100

    Add 5th day skeleton

--- src/advent/of/code/main.kt
+++ src/advent/of/code/main.kt
@@ -3,11 +3,12 @@ package advent.of.code
 //import advent.of.code.y2024.*
 import advent.of.code.y2025.*
 
-const val DAY = "4";
+const val DAY = "5";
 
 fun main() {
     if (p1.DAY == DAY) p1().solve();
     if (p2.DAY == DAY) p2().solve();
     if (p3.DAY == DAY) p3().solve();
     if (p4.DAY == DAY) p4().solve();
+    if (p5.DAY == DAY) p5().solve();
 }
